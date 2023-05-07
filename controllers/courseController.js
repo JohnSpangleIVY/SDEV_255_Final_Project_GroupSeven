caececd20e9bffaa17150e626281fe811513209f
John Spangle <jspangle2@ivytech.edu>
2023-05-07 03:21:13 -0400

    Modularized the routes even further; new  controller for the courses -JS

---
+++ controllers/courseController.js
@@ -0,0 +1,61 @@
+// Required
+const Courses = require('../models/courses');
+
+
+// List of functions: course_all , course_info , course_create_get , course_create_post , course_delete
+
+const course_all = (req, res) => {
+  Courses.find()
+    .then((result) => {
+      res.render('courses/courses', {title: 'All Courses', courses: result})
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+}
+
+const course_info = (req, res) => {
+  const id = req.params.id;
+  Courses.findById(id)
+    .then(result => {
+      res.render('courses/course-info', {course: result, title: 'Course Information'});
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+const course_create_get = (req, res) => {
+  res.render('courses/course-create', {title: 'Create a Course'});
+}
+
+const course_create_post = (req, res) => {
+  const course = new Courses(req.body);
+  course.save()
+    .then((result) => {
+      res.redirect('/courses')
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+const course_delete = (req, res) => {
+  const id = req.params.id;
+  Courses.findByIdAndDelete(id)
+    .then(result => {
+      res.json({redirect: '/courses'})
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+
+module.exports = {
+  course_all,
+  course_info,
+  course_create_get,
+  course_create_post,
+  course_delete
+}
--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -1,6 +1,6 @@
 // Required modules
 const express = require('express');
-const Courses = require('../models/courses');
+const courseController = require('../controllers/courseController');
 
 // Express router
 const router = express.Router();
@@ -8,51 +8,29 @@ const router = express.Router();
 
 // Course-related routes
 // All Courses page
-router.get('/courses', (req, res) => {
-  Courses.find()
-    .then((result) => {
-      res.render('courses', {title: 'All Courses', courses: result})
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.get('/courses', courseController.course_all);
 
-// Create a Course page
-router.get('/course-create', (req, res) => {
-  res.render('course-create', {title: 'Create a Course'});
-})
+// Create a Course page :: GET
+router.get('/course-create', courseController.course_create_get)
 
-// Individual/Selected Course :: GET  -  route
-router.get('/courses/:id', (req, res) => {
-  const id = req.params.id;
-  Courses.findById(id)
-    .then(result => {
-      res.render('course-info', {course: result, title: 'Course Information'});
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+// Individual/Selected Course :: GET
+router.get('/courses/:id', courseController.course_info);
 
-// Individual/Selected Course :: DELETE  -  route
-router.delete('/courses/:id', (req, res) => {
-  const id = req.params.id;
-  Courses.findByIdAndDelete(id)
-    .then(result => {
-      res.json({redirect: '/courses'})
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+// Individual/Selected Course :: DELETE
+router.delete('/courses/:id', courseController.course_delete);
 
-// Edit a Course page   [POSSIBLY SCRAP]
+// Create a course :: POST  -  route
+router.post('/courses', courseController.course_create_post);
+
+
+/* Possibly scrap - need to decided later ... edit functionality may be added to the course-info page instead
+
+Edit a Course page   [POSSIBLY SCRAP]
 router.get('/course-edit', (req, res) => {
   res.render('course-edit', {title: 'Edit a Course'});
 })
 
-// Edit Individual/Selected Course :: GET  -  route   [POSSIBLY SCRAP]
+Edit Individual/Selected Course :: GET  -  route   [POSSIBLY SCRAP]
 router.get('/courses/:id', (req, res) => {
   const id = req.params.id;
   Courses.findById(id)
@@ -62,24 +40,6 @@ router.get('/courses/:id', (req, res) => {
     .catch((err) => {
       console.log(err);
     });
-});
-
-// Create a course :: POST  -  route
-router.post('/courses', (req, res) => {
-  const course = new Courses(req.body);
-  course.save()
-    .then((result) => {
-      res.redirect('/courses')
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-/* may or may not need this anymore
-// Individual-Selected Course page
-app.get('/course-info', (req, res) => {
-  res.render('course-info', {title: 'Course Information'});
 });
 */
 
